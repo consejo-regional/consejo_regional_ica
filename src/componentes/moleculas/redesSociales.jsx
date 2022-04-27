ae0f5523f330cfdeb3de22469018a925063d2a1c
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-04-27 07:56:16 -0500

    avanze menu banner 2

---
+++ src/componentes/moleculas/redesSociales.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const RedesSociales = ()=> {
+      return(
+        <>
+        <div className="principal">
+            <div className='contenedor-principal'>
+                <div className='contenedor'>
+                    <div className="menu">
+                        <div className='logo'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+       
+        </>   
+      )  
+  }
+  
+  
+  export default RedesSociales;
+  
