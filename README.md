1add130c202231790f0b183c7449e826275d418c
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-04-27 01:08:34 -0500

    avanze encabezado menu principal

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# instalar sass
+npm install -D node-sass
+npm i -D sass
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/componentes/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Menu from './moleculas/menu'
+
+import "../estilos/styles.scss"
+
+
+const App = ()=> {
+      return(
+        <>
+        <Menu></Menu>
+        <h1 className="prueba">hola mundo</h1>
+        </>   
+      )  
+  }
+  
+  
+  export default App;
+  
