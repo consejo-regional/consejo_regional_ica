ed4d8be44406118fc2f61d5a5c40425d68c1833b
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-05-01 11:27:13 -0500

    netlyfy

--- src/componentes/moleculas/footerContacto.jsx
+++ src/componentes/moleculas/footerContacto.jsx
@@ -14,7 +14,7 @@ const FooterContacto = ()=> {
 
             <div className="footerContacto">
                 <div className="footerContactoDescripccion">
-                    <h4>Consejo Regional – ICA CMP</h4>
+                    <h4>Consejo Regional – ICA CMP  vvvv</h4>
                     <h3>El Colegio Médico del Perú (CMP) es una institución autónoma de derecho público interno, representativa de la profesión médica en todo el territorio de la República. El Consejo Regional III de Lima (CR III) es el órgano directivo del CMP en Lima Metropolitana y representa a más del 50% de los médicos colegiados del Perú.</h3>
                     <h4>Contáctenos</h4>
                     <h4>Malecón de la Reserva 791</h4>
