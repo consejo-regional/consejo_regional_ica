ca880c1a7f3571bdc7c4da03300598464a92063f
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-05-01 22:41:41 -0500

    menu responsive avanze

--- src/componentes/moleculas/menuItem.jsx
+++ src/componentes/moleculas/menuItem.jsx
@@ -1,122 +1,153 @@
-import React from 'react'
+import React,{useEffect,useRef} from 'react'
 import {Link} from 'react-router-dom'
 
 
+
 const MenuItem = ()=> {
+
+    const boton=useRef()
+
+
+    const click=()=>{
+        
+        const prueba=boton.current
+        prueba.classList.toggle('show')
+        console.log(boton.current)
+
+
+    }
+
+    useEffect(()=>{
+        // const prueba=boton.current
+        // prueba.addEventListener('click',()=>{
+        //     console.log('accedi con la referencia')
+        //     prueba.classlist.toggle('show')
+        //     console.log(prueba)
+        // })
+
+    },[])
       return(
         <>
       
-    <header classNameN="header">
-        <nav className="nav">
+    <header className="header">
+        {/* <div>dvsdfvdfsdf</div> */}
+
+        <div className="header-conteiner">
+        
+        <div   onClick={click} className="boton-menu-hamburguesa show"></div>
+        <nav className="nav " ref={boton}>
             <ul className="menuu ">
+                <div   onClick={click} className="boton-menu-hamburguesa show"></div>
+
                 <li className="item itemMenu">
-                    <Link to="/inicio" className="link">INICIO </Link>
+                    <Link to="/inicio"  onClick={click}  className="link">INICIO </Link>
                    
                 </li>
                 <li className="item">
-                    <Link to="/nosotros" className="link">NOSOTROS</Link>
+                    <Link to="/nosotros" onClick={click} className="link">NOSOTROS</Link>
                     <ul className="menuu submenu">
                         <li className="item">
-                            <Link to="" className="link">BIENVENIDA</Link>
+                            <Link to="" onClick={click} className="link linkSubmenu">BIENVENIDA</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">INSTITUCION</Link>
+                            <Link to="" className="link linkSubmenu">INSTITUCION</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">JUNTA DIRECTIVA</Link>
-                            <ul className="menuu submenu">
+                            <Link to="" className="link linkSubmenu">JUNTA DIRECTIVA</Link>
+                            {/* <ul className="menuu submenu">
                                 <li className="item"><Link to="" className="link">subsubmenu1</Link></li>
                                 <li className="item"><Link to="" className="link">subsubmenu1</Link></li>
                                 <li className="item"><Link to="" className="link">sub submenu1</Link></li>
                                 <li className="item"><Link to="" className="link">subsubmenu1</Link></li>
-                            </ul>
+                            </ul> */}
                         </li>
                         
                         <li className="item">
-                            <Link to="" className="link">SOCIEDADES CIENTIFICAS</Link>
+                            <Link to="" className="link linkSubmenu">SOCIEDADES CIENTIFICAS</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">    COMITES    </Link>
+                            <Link to="" className="link linkSubmenu">    COMITES    </Link>
                         </li>
                     </ul>
                 </li>
                 <li className="item">
-                    <Link to="/tramites" className="link">TRAMITES</Link>
+                    <Link to="/tramites"  onClick={click}  className="link">TRAMITES</Link>
                     <ul className="menuu submenu">
                         <li className="item">
-                            <Link to="" className="link">CONSTANCIA DE HABILIDAD</Link>
+                            <Link to="" className="link linkSubmenu">CONSTANCIA DE HABILIDAD</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">INICIAR COLEGIATURA</Link>
+                            <Link to="" className="link linkSubmenu">INICIAR COLEGIATURA</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">RECTIFICACION</Link>
+                            <Link to="" className="link linkSubmenu">RECTIFICACION</Link>
                         </li>
                         
                         <li className="item">
-                            <Link to="" className="link">ACTUALIZACION DE DATOS</Link>
+                            <Link to="" className="link linkSubmenu">ACTUALIZACION DE DATOS</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">CONSTANCIA DE ETICA</Link>
+                            <Link to="" className="link linkSubmenu">CONSTANCIA DE ETICA</Link>
                         </li>
                     </ul>
                 </li>
                 <li className="item">
-                    <Link to="/servicios" className="link">SERVICIOS</Link>
+                    <Link to="/servicios"  onClick={click}  className="link">SERVICIOS</Link>
                     <ul className="menuu submenu">
                         <li className="item">
-                            <Link to="" className="link">BOLSA DE TRABAJO</Link>
+                            <Link to="" className="link linkSubmenu">BOLSA DE TRABAJO</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">DIFUSION</Link>
+                            <Link to="" className="link linkSubmenu">DIFUSION</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">CONOCE A TU MEDICO</Link>
+                            <Link to="" className="link linkSubmenu">CONOCE A TU MEDICO</Link>
                         </li>
                     </ul>
                     </li>
                 <li className="item">
-                    <Link to="/comunicaciones" className="link">COMUNICACIONES</Link>
+                    <Link to="/comunicaciones"  onClick={click}  className="link">COMUNICACIONES</Link>
                     <ul className="menuu submenu">
                         <li className="item">
-                            <Link to="" className="link">OPINION Y PRONUNCIAMIENTO</Link>
+                            <Link to="" className="link linkSubmenu">OPINION Y PRONUNCIAMIENTO</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">DIFUSIONES                 </Link>
+                            <Link to="" className="link linkSubmenu">DIFUSIONES                 </Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">BOLETIN INFORMATIVO</Link>
+                            <Link to="" className="link linkSubmenu">BOLETIN INFORMATIVO</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">NOTICIAS                      </Link>
+                            <Link to="" className="link linkSubmenu">NOTICIAS                      </Link>
                         </li>
                     </ul>
                 </li>
                 <li className="item">
-                    <Link to="/eventos" className="link">EVENTOS</Link>
+                    <Link to="/eventos"  onClick={click}  className="link">EVENTOS</Link>
                     <ul className="menuu submenu">
                         <li className="item">
-                            <Link to="" className="link">CURSOS                </Link>
+                            <Link to="" className="link linkSubmenu">CURSOS                </Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">TALLERES             </Link>
+                            <Link to="" className="link linkSubmenu">TALLERES             </Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">CELEBRACIONES</Link>
+                            <Link to="" className="link linkSubmenu">CELEBRACIONES</Link>
                         </li>
                         <li className="item">
-                            <Link to="" className="link">CAMPAÑAS MEDICAS</Link>
+                            <Link to="" className="link linkSubmenu">CAMPAÑAS MEDICAS</Link>
                         </li>
                     </ul>
                 </li>
                 <li className="item">
-                    <Link to="/beneficios" className="link">BENEFICIOS</Link>
+                    <Link to="/beneficios"  onClick={click}  className="link">BENEFICIOS</Link>
                 </li>
                 <li className="item">
-                    <Link to="/centroRecreaciones" className="link">CENTRO DE RECREACIONES</Link>
+                    <Link to="/centroRecreaciones"  onClick={click}  className="link">CENTRO DE RECREACIONES</Link>
                 </li>
             </ul>
         </nav>
+        </div>
     </header>
 
         </>   
