46434f194b05ed95f4fa24aea25ec2431f6b03da
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-06-02 21:08:44 -0500

    modificando banner de inicio

--- src/componentes/moleculas/anuncios.jsx
+++ src/componentes/moleculas/anuncios.jsx
@@ -17,8 +17,10 @@ const Anuncios = ()=> {
       return(
         <>
         <div className="contenedor_anuncios" ref={botonEsconder}>
-            <div className='anuncios_banner'>
+            <div className="anuncios_conteiner">
+                <img alt='' className='anuncios_banner' src={process.env.PUBLIC_URL+`/imagenes/Pronunciamiento.jpg`}></img>
                 <div className="anuncios_banner_boton"  onClick={clickAnuncio}  ></div>
+
             </div>
         </div>
         </>   
