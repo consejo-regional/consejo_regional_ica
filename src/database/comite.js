1155f49cf3527f4945734a9bc45f2d6f935dcd1a
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-06-02 21:44:50 -0500

    añadiendo comite a la base de datos

---
+++ src/database/comite.js
@@ -0,0 +1,280 @@
+
+const ComitesArray=[
+    {
+        titulo:"COMITÉ DE VIGILANCIA ÉTICA Y DEONTOLOGÍA",
+        presidente:"DR. AUDIAS JOSUE PEREZ AGUIRRE	",
+        miembro1:{
+            nombre:"DRA. MARIA DEL CARMEN VARGAS GARCIA",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro2:{
+            nombre:"DR. CARLOS HUGO GUERRERO CHACALTANA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro3:{
+            nombre:"DR. ADRIAN RODOLFO PALOMINO HERENCIA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+    },
+    {
+        titulo:"COMITÉ DE ASUNTOS CONTENCIOSOS Y PROCEDIMIENTOS DISCIPLINARIOS",
+        presidente:"DRA. FLOR DE MARÍA MEJÍA ESCATE	",
+        miembro1:{
+            nombre:"DRA. TERESA DE LOS MILAGROS ANDRADE SOTIL",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro2:{
+            nombre:"DR. JONY HUBER HUARIPAUCAR MISAICO",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro3:{
+            nombre:"DR. RAÚL SANTA MARIA RÍOS",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+    },
+    {
+        titulo:"COMITÉ DE EVENTOS CIENTÍFICOS E INFORMES TÉCNICOS",
+        presidente:"DR. ALEXANDER HUMBERTO PECHO PECHE",
+        miembro1:{
+            nombre:"DR. ROGGER ANTONIO ABREGU AGUADO",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro2:{
+            nombre:"DR. LUIS GIANCARLO SARAVIA HUARCA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro3:{
+            nombre:"DR. MANUEL JOSE JHONG CASTRO",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+    },
+    {
+        titulo:"COMITÉ DE INVESTIGACIÓN E INNOVACIÓN",
+        presidente:"DR. LUÍS CARLOS OLIVERA RAMOS ",
+        miembro1:{
+            nombre:"DR. PATRICIA CAROLINA HUAMAN OROSCO",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro2:{
+            nombre:"DR. JAVIER EDUARDO URIBE GODOY ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro3:{
+            nombre:"DR. JESUS ANDREE NEYRA LEÓN",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro4:{
+            nombre:"CANDY MILAGROS CORNEJO CHACALTANA",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro5:{
+            nombre:"DR. CARLOS ALBERTO DÁVILA HERNANDEZ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro6:{
+            nombre:"DRA. GIANNY DENISSE GALAGARZA GUTIÉRREZ",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro7:{
+            nombre:"DR. JAVIER ALFREDO GRADOS TELLO",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro8:{
+            nombre:"DR. LUIS GIANCARLO SARAVIA HUARCA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro9:{
+            nombre:"DR. JOHN ALBERTO PALOMINO MONTEROLA ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro10:{
+            nombre:"DR. JUAN CARLOS DÍAZ MONGE",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro11:{
+            nombre:"DR. CARLOS ANDRÉS CARRASCO FARFAN ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro12:{
+            nombre:"ATOCHE MENDOZA JOSE ALEXANDER",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro13:{
+            nombre:"VIVANCO RAMOS LUIS GUILLERMO VLADIMIR",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro14:{
+            nombre:"YAURI GARAVITO DIANA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        }
+    },
+    {
+        titulo:"COMITÉ DE LUCHA CONTRA EL EJERCICIO ILEGAL DE LA MEDICINA",
+        presidente:"DR. ALEX ALBERTO GUIBOVICH MESINAS	",
+        miembro1:{
+            nombre:"DRA. FRANCESCA ALICE DUARTE PORTUGAL	",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro2:{
+            nombre:"DR. ALAN MARCO CHAVESTA CUSTODIO",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro3:{
+            nombre:"DR. MARCO ANTONIO GUERRERO ANGULO",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+    }
+    ,
+    {
+        titulo:"COMITÉ EDUCACIÓN MÉDICA CONTINUA",
+        presidente:"DR. ALEXANDER HUMBERTO PECHO PECHE	",
+        miembro1:{
+            nombre:"DRA. JULIANA MARIA APARCANA MACHADO	",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro2:{
+            nombre:"DR. ROGGER ANTONIO ABREGU AGUADO",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro3:{
+            nombre:"DR. LUIS GIANCARLO SARAVIA HUARCA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro4:{
+            nombre:"DR. MANUEL JOSE JHONG CASTROA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro5:{
+            nombre:"DR. FRANCO EDGARD MIO PALACIOS",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+    }
+    ,
+    {
+        titulo:"COMITÉ DE MEDICINA TRADICIONAL, ALTERNATIVA Y COMPLEMENTARIA",
+        presidente:"DRA. JUANA MARÍA VEGA CARLOS	",
+        miembro1:{
+            nombre:"DRA. ZOILA AURORA LENGUA KUAN	",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro2:{
+            nombre:"DRA. PETRONILA AURORA CASTRO ROJO",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro3:{
+            nombre:"DRA. NIEVES ETHEL MORALES MEZA ",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro4:{
+            nombre:"DR. PEDRO CESAR ARANZABAL NAVARRETE",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        }
+    }
+    ,
+    {
+        titulo:"COMITÉ DE DEPORTES",
+        presidente:"DR. FELIX ENRIQUE MARTINEZ PAUCAR ",
+        miembro1:{
+            nombre:"DR. CRISTHIAN RAUL PALACIOS NEYRA",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro2:{
+            nombre:"DRA. SONIA CHOQUE GUTIERREZ",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro3:{
+            nombre:"DRA. JULIANA MARIA APARCANA MACHADO ",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro4:{
+            nombre:"DR. JESUS ROBERTO CASTILLO HUASASQUICHE ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro5:{
+            nombre:"DR. ANDRES GUIDO BENDEZU MARTINEZ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro6:{
+            nombre:"DR. PEDRO PABLO ZEVALLOS TORRES",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro7:{
+            nombre:"DR. VICTOR PERCY DE LA CRUZ TORREALVA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro8:{
+            nombre:"DR. SANDRO JAVIER MUÑOZ BENDEZU ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro9:{
+            nombre:"DR. FRANCO FAJARDO HAROLD CHRISTIAN ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro10:{
+            nombre:"DR. MIJAIL VLADIMIR LLONA GARCIA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro11:{
+            nombre:"DRA. EDITH PAREJA MALDONADO ",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro12:{
+            nombre:"DRA. CECILIA PARDO ALEJO",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        }
+    } ,
+    {
+        titulo:"COMITÉ DE SALUD PÚBLICA",
+        presidente:"DR. LUIS FELIPE MUÑANTE APARCANA",
+        miembro1:{
+            nombre:"DR. RICARDO MIGUEL CABRERA CASTILLO",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro2:{
+            nombre:"DR. JOSE DANIEL HERNÁNDEZ ZÚÑIGA",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro3:{
+            nombre:"DR. ROLANDO MANUEL ANICAMA ZAPATA ",
+            foto:process.env.PUBLIC_URL + `/imagenes/mujer.png`
+        },
+        miembro4:{
+            nombre:"DRA. LITA SILVIA DEL RIO MUÑIZ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro5:{
+            nombre:"DR. RÓMULO DANIEL CAHUA VALDIVIESO",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro6:{
+            nombre:"DR. JUAN ALBERTO BORJAS LENGUA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro7:{
+            nombre:"DR. MARCOS RUPERTO CABRERA PIMENTEL",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro8:{
+            nombre:"DR. JORGE ENRRIQUE TORRES MELGAR",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        }
+    },
+    {
+        titulo:"COMITÉ DE ECONOMÍA",
+        presidente:"DRA. FLOR ELIZABETH AVILES BONIFAZ",
+        miembro1:{
+            nombre:"DRA. YEIMING MARTINELLY ROJAS VENTURA ",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro2:{
+            nombre:"DR. WALTER MILTON FUENTES TANG",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+        miembro3:{
+            nombre:"DR. JESÚS GABRIEL TOVAR HINOSTROZA",
+            foto:process.env.PUBLIC_URL + `/imagenes/hombre.png`
+        },
+    }
+]
+
+export default ComitesArray
