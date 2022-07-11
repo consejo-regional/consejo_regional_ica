13419ebfd722b4e52bf53bd6df1a63bfd7b57ae7
consejo.regional.ica <consejo.regional.ica@cmp.org.pe>
2022-07-11 17:20:36 -0500

    agregando nuevo banner, actualizando el servicios

--- src/componentes/moleculas/pronunciamientos.jsx
+++ src/componentes/moleculas/pronunciamientos.jsx
@@ -18,11 +18,13 @@ const Anuncios = ()=> {
         <>
         <div className="contenedor_anuncios" ref={botonEsconder}>
             <div className="anuncios_conteiner">
-                <img alt='' className='anuncios_banner' src={process.env.PUBLIC_URL+`/pronunciamiento/Pronunciamiento.jpg`}></img>
+                <img alt='' className='anuncios_banner' src={process.env.PUBLIC_URL+`/pronunciamiento/Pronunciamiento.jpeg`}></img>
                 <div className="anuncios_banner_boton"  onClick={clickAnuncio}  ></div>
-
+                <img alt='' className='anuncios_banner' src={process.env.PUBLIC_URL+`/pronunciamiento/Pronunciamiento12.jpeg`}></img>
+                <div className="anuncios_banner_boton"  onClick={clickAnuncio}  ></div>
             </div>
         </div>
+        
         </>   
       )  
   }
