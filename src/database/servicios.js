59e7047f3da31ae7a2b3827f7668381b58b6a98b
consejo.regional.ica <consejo.regional.ica@cmp.org.pe>
2022-08-03 16:08:43 -0500

    actualizando servicios

--- src/database/servicios.js
+++ src/database/servicios.js
@@ -1,32 +1,36 @@
 
 
-const Servicios=[{
-  imagen:process.env.PUBLIC_URL + `/imagenes/Botones-servicios_CMP-Digital (1).webp`,
-  nombre:"SISTCERE",
-  description:"",
-  enlace:"https://www.cmp.org.pe/sistcere-2/"
-},{
-  imagen:process.env.PUBLIC_URL + `/imagenes/Botones-servicios_CMP-Digital (1).webp`,
-  nombre:"SEMEFA",
-  description:"",
-  enlace:"https://www.cmp.org.pe/semefa/"
-},{
-  imagen:process.env.PUBLIC_URL + `/imagenes/Botones-servicios_Autenticidad-constancia-de-habilidad.webp`,
-  nombre:"FOSEMED",
-  description:"",
-  enlace:"https://www.cmp.org.pe/fosemed/"
-},{
-  imagen:process.env.PUBLIC_URL + `/imagenes/Botones-servicios_Autenticidad-constancia-de-habilidad.webp`,
-  nombre:"DEFENSORIA DEL MEDICO",
-  description:"",
-  enlace:"https://www.cmp.org.pe/defensoria-del-medico/"
-},{
-  imagen:process.env.PUBLIC_URL + `/imagenes/Botones-servicios_Autenticidad-constancia-de-habilidad.webp`,
-  nombre:"CONOCE A TU MEDICO",
-  description:"",
-  enlace:"https://www.cmp.org.pe/conoce-a-tu-medico/"
-}
-]
 
+  
+
+  const Servicios=[{
+    imagen:process.env.PUBLIC_URL + `/imagenes/IMAGE-1.png`,
+    nombre:"SISTCERE",
+    description:"",
+    enlace:"https://www.cmp.org.pe/sistcere-2/"
+  },{
+    imagen:process.env.PUBLIC_URL + `/imagenes/IMAGE-2.png`,
+    nombre:"SEMEFA",
+    description:"",
+    enlace:"https://www.cmp.org.pe/semefa/"
+  },{
+    imagen:process.env.PUBLIC_URL + `/imagenes/IMAGE-4.png`,
+    nombre:"FOSEMED",
+    description:"",
+    enlace:"https://www.cmp.org.pe/fosemed/"
+  },{
+    imagen:process.env.PUBLIC_URL + `/imagenes/IMAGE-5.png`,
+    nombre:"DEFENSORIA DEL MEDICO",
+    description:"",
+    enlace:"https://www.cmp.org.pe/defensoria-del-medico/"
+  },{
+    imagen:process.env.PUBLIC_URL + `/imagenes/IMAGE-3.png`,
+    nombre:"CONOCE A TU MEDICO",
+    description:"",
+    enlace:"https://www.cmp.org.pe/conoce-a-tu-medico/"
+  }
+  ]
+  
+  
 
 export default Servicios
