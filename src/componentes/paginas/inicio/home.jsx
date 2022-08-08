8f8665fafe0a47fd050c5cd6fe6ee64b42bed29b
alvaro ccoyllo <pedro_alvaro@outlook.com.pe>
2022-08-07 23:10:28 -0500

    agragando autoplay al slider principal

--- src/componentes/paginas/inicio/home.jsx
+++ src/componentes/paginas/inicio/home.jsx
@@ -2,108 +2,81 @@ import React  from 'react'
 
 import SericiosGrid from '../servicios/serviciosGrid'
 import FooterContacto from '../../componente/footer/footerContacto'
-
-import  Slider from '../../componente/slider/Slider'
-
-
 import {useEffect,useState} from "react"
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Publicaciones from './publicacionesDestacadas'
 import PalabrasDecano from './palabrasDecano'
 import NoticiasDestacadas from './noticiasDestacadas'
-
 import AnimatedNumbers from "react-animated-numbers";
+import {Slideshow, Slide} from '../../componente/slider/sliderautoplayinicio'
+import SliderPrincipal from '../../../data/sliderPrincipal';
 
+
+
+const informacion=SliderPrincipal
 
 
 const Home = ()=> {
-
-
   const[contador,setContador]=useState()
-
-
-  // coonst [num, setNum] = useState(331231);
-
-
    useEffect(()=>{
    AOS.init({duration: 1000})
    fetch("https://api.countapi.xyz/update/cmpica.org.pe/llavereact/?amount=1")
    .then(response=>response.json())
    .then(
     data=>setContador(data.value)
-   )
-
-
-
-   },[])
+   )},[])
 
       return(
         <>
         
+        <main className="slider-principal">
+          <Slideshow controles={true} autoplay={true} velocidad="6000" intervalo="3000">
+                  {
+                     informacion.map(c=>(
+                          <Slide>
+                                  <img  src={process.env.PUBLIC_URL + c.ruta} alt=""/>
+                          </Slide>
+                     ))
+                  }  
+          </Slideshow>
+		   </main>
 
+        <br></br>
+        <br></br>
 
-
-
-
-        <Slider></Slider>
-
-<br></br>
-<br></br>
-
-<div className="contenedorContadorVisitas">
-      <div className="countdown">
-              <div className="box">
-                  <span className="text">Esta pagina ha sido vista</span>
-                  <span className="num" id="day-box">
-                        <AnimatedNumbers
-                              animateToNumber={contador}
-                              fontStyle={{ fontSize: 55 }}
-                              configs={
-                                [{"mass":1,"tension":30,"friction":10},{"mass":2,"tension":40,"friction":10},{"mass":3,"tension":30,"friction":10}]
-                              }   
-                            >
-                      </AnimatedNumbers>   
-                  </span>
-                  <span className="text">VECES</span>
-              </div> 
-      </div>
-</div>
+        <div className="contenedorContadorVisitas">
+              <div className="countdown">
+                      <div className="box">
+                          <span className="text">Esta pagina ha sido vista</span>
+                          <span className="num" id="day-box">
+                                <AnimatedNumbers
+                                      animateToNumber={contador}
+                                      fontStyle={{ fontSize: 55 }}
+                                      configs={
+                                        [{"mass":1,"tension":30,"friction":10},{"mass":2,"tension":40,"friction":10},{"mass":3,"tension":30,"friction":10}]
+                                      }   
+                                    >
+                              </AnimatedNumbers>   
+                          </span>
+                          <span className="text">VECES</span>
+                      </div> 
+              </div>
+        </div>
 
  
         <div data-aos="fade-up">
-
-        <SericiosGrid></SericiosGrid> 
-
-        
+          <SericiosGrid></SericiosGrid> 
         </div>
-
-
-
-
-{/* <img alt="contador de visitas" src="https://websmultimedia.com/contador-de-visitas.php?id=4466"></img> */}
-
-
-  
-
-
-
 
 
         <div className="contenedor-separador-bloque">
             <hr className="separador-bloque"></hr>
         </div>
 
-
-
         <div data-aos="fade-up">
-
-
           <Publicaciones></Publicaciones>
         </div>
-
-
-
 
         <div className="contenedor-separador-bloque">
             <hr className="separador-bloque"></hr>
@@ -115,10 +88,7 @@ const Home = ()=> {
             <hr className="separador-bloque"></hr>
         </div>
 
-
-
         <NoticiasDestacadas></NoticiasDestacadas>
-
 
         <div data-aos="fade-up">
         <FooterContacto></FooterContacto> 
---
+++ src/data/sliderPrincipal.js
@@ -0,0 +1,15 @@
+
+  const SliderPrincipal=[{
+    ruta:"Slider/img1.jpg"
+  },{
+    ruta:"Slider/img2.jpg"
+  },{
+    ruta:"Slider/img3.jpg"
+  },{
+    ruta:"Slider/img4.jpg"
+  }
+  ]
+  
+  
+
+export default SliderPrincipal
