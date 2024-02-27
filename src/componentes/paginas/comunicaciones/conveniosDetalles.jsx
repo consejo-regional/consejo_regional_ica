4540dd09e1219a8c32df9a81cb83dbf072660af1
pedro alvaro zegel <pedro_alvaro@outlook.com.pe>
2024-02-27 15:49:04 -0500

    añadiendo ventana flotante a convenios

--- src/componentes/paginas/comunicaciones/conveniosDetalles.jsx
+++ src/componentes/paginas/comunicaciones/conveniosDetalles.jsx
@@ -1,8 +1,8 @@
-import React  from 'react'
+import React ,{useRef} from 'react'
+import {useEffect,useState} from "react"
 
 import {useParams} from 'react-router-dom'
 // import EfemeridesInformacion from  "../../../data/efemerides"
-import {useEffect,useState} from "react"
 
 
 
@@ -41,32 +41,52 @@ import {useEffect,useState} from "react"
 
     const ConvenioItem = ({imagenes,description, tipo,contenido,fecha})=> {
   
+
+          const [mostrarHTML, setMostrarHTML] = React.useState(false);
+          const [parametro, setParametro] = React.useState('');
+          const botonEsconderFlotanteOpinion=useRef()
+          const clickFlotanteEsconder=()=>{
+              const BotonEsconder_1=botonEsconderFlotanteOpinion.current
+              BotonEsconder_1.classList.add('esconder_1')
+              setMostrarHTML(false);
+          }
+          const clickFlotante = (param) => {
+              setParametro(param);
+              setMostrarHTML(true);
+            };
   
         if(tipo==="CONVENIO"){
           return(
             <>
-            <div className="detalle-grid">
-                <h2>NUESTROS CONVENIOS</h2>
-                <div className="detalle-grid-contenido" >
-                <div className="detalle-cart-contenido">
-                    <div className="detalle-cart-contenido-titulo">{description}</div>
-                    <div className="detalle-cart-contenido-fecha">{fecha}</div>
-                    <div className="">{contenido}</div>
+              <div className="detalle-grid">
+                  <h2>NUESTROS CONVENIOS</h2>
+                  <div className="detalle-grid-contenido" >
+                  <div className="detalle-cart-contenido">
+                      <div className="detalle-cart-contenido-titulo">{description}</div>
+                      <div className="detalle-cart-contenido-fecha">{fecha}</div>
+                      <div className="">{contenido}</div>
+                    
+                    </div>
+                    <div className="detalle-grid-contenido-imagenes">
+                      {
+                          imagenes.split(';').map(c=>(
+                              <img className="detalle-grid-contenido-imagem" src={process.env.PUBLIC_URL+`/`+ c} alt={c} onClick={() => clickFlotante(c)}  ></img> 
+
+                        ))
+                      }
+
+                    </div>
                   
                   </div>
-                  <div className="detalle-grid-contenido-imagenes">
-                    {
-                        imagenes.split(';').map(c=>(
-                            <img className="detalle-grid-contenido-imagem" src={process.env.PUBLIC_URL+`/`+ c} alt={c}></img> 
-
-                      ))
-                    }
-
-                  </div>
-                 
-                </div>
-            </div>
-            
+              </div>
+              {mostrarHTML && (
+               <div className="Flotante_contenedor" ref={botonEsconderFlotanteOpinion}>
+                   <div className="Flotante_tarjeta">
+                       <div className="anuncios_banner_boton_1" onClick={clickFlotanteEsconder} ></div> 
+                       <img className={`Flotante_imagen`} alt="" src={process.env.PUBLIC_URL + "/" + parametro }  ></img>
+                   </div>
+               </div>
+               )}
             </>   
           )  
   
