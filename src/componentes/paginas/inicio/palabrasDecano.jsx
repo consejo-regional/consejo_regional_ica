f0f7d618115c4ac7c7e2d8d5292426cdfe231d00
pedro alvaro ccoyllo condori <pedro_alvaro@outlook.com.pe>
2024-03-08 12:05:29 -0500

    terminando palabras del decano(varios mensajes)

--- src/componentes/paginas/inicio/palabrasDecano.jsx
+++ src/componentes/paginas/inicio/palabrasDecano.jsx
@@ -15,14 +15,9 @@ const PalabrasDecano = ()=> {
         );
     },[])
 
-
-
-
       return(
         <>
-
-
-        <div className="contenedor-serviciosGrid-titulo">
+            <div className="contenedor-serviciosGrid-titulo">
                 <div className="contenedor-serviciosGrid-logo">
                     <div className="logo-colegio-svg"></div>
                 </div>
@@ -35,27 +30,27 @@ const PalabrasDecano = ()=> {
             </div>
 
             <div className="container-palabras-decano">
-                 <div className="container-palabras-decano-contenido">
-                       <ReactPlayer
-                            className='react-player'
-                            url={informacion[0].url}
-                            width='auto'
-                            height='50vh'
-                            controls={true}
-                        />
-                        <div className="container-palabras-decano-blur">
-                            <img src={process.env.PUBLIC_URL +'/imagenes/pngegg.png'} style={{width: 'auto', height: '5rem'}}></img>
-                            <p  className="container-palabras-decano-blur">
-                                {informacion[0].descripccion}
-                           
-                            </p>
-                        </div>
-                </div>
+                    {
+                        informacion.map(c=>(
+                            <div className="container-palabras-decano-contenido">
+                                <ReactPlayer
+                                    className='react-player'
+                                    url={c.url}
+                                    width='100%'
+                                    height='auto'
+                                    controls={true}
+                                />
+                                <div className="container-palabras-decano-blur">
+                                    <img src={process.env.PUBLIC_URL +'/imagenes/pngegg.png'} style={{width: 'auto', height: '5rem'}}></img>
+                                    <p  className="container-palabras-decano-blur">
+                                        {c.descripccion}
+                                    </p>
+                                </div>
+                            </div>
+                        ))
+                    }
             </div>
-
-       
-
-        </>   
+        </>  
       )  
   }
   
